35ce6d072222f5b03671eb16399dd4ac19cd6565
huynhnhan15 <nhanhuynh959595@gmail.com>
2025-09-26 20:52:27 +0700

    nop bai

--- demo_thymeleaf/src/main/java/com/codegym/demo_thymeleaf/repository/PlayerRepository.java
+++ demo_thymeleaf/src/main/java/com/codegym/demo_thymeleaf/repository/PlayerRepository.java
@@ -5,6 +5,7 @@ import com.codegym.demo_thymeleaf.entity.Player;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -28,6 +29,8 @@ public class    PlayerRepository implements IPlayerRepository {
         return player;
     }
     @Override
+    @Transactional
+
     public boolean add(Player player) {
         try{
             entityManager.persist(player);
